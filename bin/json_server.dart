ea4dda71304ec7983fd811b5adf9bb3f35cb7a96
hariton1500 <hariton1500@gmail.com>
2022-09-30 12:29:38 +0300

    234

--- bin/json_server.dart
+++ bin/json_server.dart
@@ -2,5 +2,5 @@ import 'package:json_server/json_server.dart' as json_server;
 
 void main(List<String> arguments) async {
   print('Json server runnning...');
-  await json_server.run();
+  print(await json_server.run());
 }
--- lib/json_server.dart
+++ lib/json_server.dart
@@ -3,14 +3,14 @@ import 'dart:io';
 import 'package:path/path.dart' as path;
 
 import 'Models/cable.dart';
-import 'Models/data.dart';
 import 'Models/fosc.dart';
 import 'Models/node.dart';
 
 Future<String> run() async {
-  List<Fosc> fileFoscsContent = [];
-  List<Node> fileNodesContent = [];
-  List<Cable> fileCablesContent = [];
+  Map<String, Fosc> fileFoscsContent = {};
+  Map<String, Node> fileNodesContent = {};
+  Map<String, Cable> fileCablesContent = {};
+
   HttpServer server;
 
   final targetFoscsFile = File(
@@ -22,36 +22,29 @@ Future<String> run() async {
 
   if (await targetFoscsFile.exists()) {
     print('Serving data from $targetFoscsFile');
-    fileFoscsContent =
-        (jsonDecode(await targetFoscsFile.readAsString()) as List)
-            .map((e) => Fosc.fromJson(e))
-            .toList();
+    //print(jsonDecode(await targetFoscsFile.readAsString()));
+    fileFoscsContent = (jsonDecode(await targetFoscsFile.readAsString()) as Map).map((key, value) => MapEntry<String, Fosc>(key.toString(), Fosc.fromJson(value)));
+    //fileFoscsContent = jsonDecode(await targetFoscsFile.readAsString()) as Map<String, Fosc>;
   } else {
     print("$targetFoscsFile doesn't exists, nothing to load");
   }
 
   if (await targetNodesFile.exists()) {
     print('Serving data from $targetNodesFile');
-    fileNodesContent =
-        (jsonDecode(await targetNodesFile.readAsString()) as List)
-            .map((e) => Node.fromJson(e))
-            .toList();
+    fileNodesContent = jsonDecode(await targetNodesFile.readAsString());
   } else {
     print("$targetNodesFile doesn't exists, nothing to load");
   }
 
   if (await targetCablesFile.exists()) {
     print('Serving data from $targetCablesFile');
-    fileCablesContent =
-        (jsonDecode(await targetCablesFile.readAsString()) as List)
-            .map((e) => Cable.fromJson(e))
-            .toList();
+    fileCablesContent = jsonDecode(await targetCablesFile.readAsString());
   } else {
     print("$targetCablesFile doesn't exists, nothing to load");
   }
 
   try {
-    server = await HttpServer.bind(InternetAddress.loopbackIPv4, 4044);
+    server = await HttpServer.bind(InternetAddress.anyIPv4, 4044);
   } catch (e) {
     print("Couldn't bind to port 4044: $e");
     exit(-1);
@@ -72,31 +65,21 @@ Future<String> run() async {
         case 'list':
           print('listing foscs=$fileFoscsContent');
           req.response
-              .writeAll(fileFoscsContent.map((e) => json.encode(e)), '\n');
+              .writeAll(fileFoscsContent.values.map((e) => json.encode(e)), '\n');
           await req.response.close();
           break;
         case 'add':
           print('adding new fosc');
-          fileFoscsContent.add(Fosc.fromJson(
+          fileFoscsContent[req.requestedUri.queryParameters['key']!] = (Fosc.fromJson(
               json.decode(Utf8Decoder().convert(await req.last))));
           targetFoscsFile.writeAsString(json.encode(fileFoscsContent));
           break;
         case 'put':
-          String jsonString = '';
-          try {
-            print(
-                'putting fosc with key=${req.requestedUri.queryParameters['key']}');
-            jsonString = Utf8Decoder().convert(await req.last);
-            fileFoscsContent[fileFoscsContent.indexWhere((fosc) =>
-                    fosc.key == req.requestedUri.queryParameters['key'])] =
-                Fosc.fromJson(json.decode(jsonString));
-            targetFoscsFile.writeAsString(json.encode(fileFoscsContent));
-          } catch (e) {
-            print('fosc was not found with that key');
-            print('adding new fosc');
-            fileFoscsContent.add(Fosc.fromJson(json.decode(jsonString)));
-            targetFoscsFile.writeAsString(json.encode(fileFoscsContent));
-          }
+          print(
+              'putting fosc with key=${req.requestedUri.queryParameters['key']}');
+          fileFoscsContent[req.requestedUri.queryParameters['key']!] = (Fosc.fromJson(
+            json.decode(Utf8Decoder().convert(await req.last))));
+          targetFoscsFile.writeAsString(json.encode(fileFoscsContent));
           break;
       }
     }
@@ -106,31 +89,21 @@ Future<String> run() async {
         case 'list':
           print('listing nodes');
           req.response
-              .writeAll(fileNodesContent.map((e) => json.encode(e)), '\n');
+              .writeAll(fileNodesContent.values.map((e) => json.encode(e)), '\n');
           await req.response.close();
           break;
         case 'add':
           print('adding new node');
-          fileNodesContent.add(Node.fromJson(
+          fileNodesContent[req.requestedUri.queryParameters['key']!] = (Node.fromJson(
               json.decode(Utf8Decoder().convert(await req.last))));
           targetNodesFile.writeAsString(json.encode(fileNodesContent));
           break;
         case 'put':
-          String jsonString = '';
-          try {
-            print(
-                'putting node with key=${req.requestedUri.queryParameters['key']}');
-            jsonString = Utf8Decoder().convert(await req.last);
-            fileNodesContent[fileNodesContent.indexWhere((node) =>
-                    node.key == req.requestedUri.queryParameters['key'])] =
-                Node.fromJson(json.decode(jsonString));
-            targetNodesFile.writeAsString(json.encode(fileNodesContent));
-          } catch (e) {
-            print('node was not found with that key');
-            print('adding new node');
-            fileNodesContent.add(Node.fromJson(json.decode(jsonString)));
-            targetNodesFile.writeAsString(json.encode(fileNodesContent));
-          }
+          print(
+              'putting node with key=${req.requestedUri.queryParameters['key']}');
+          fileNodesContent[req.requestedUri.queryParameters['key']!] = (Node.fromJson(
+              json.decode(Utf8Decoder().convert(await req.last))));
+          targetNodesFile.writeAsString(json.encode(fileNodesContent));
           break;
       }
     }
@@ -141,60 +114,27 @@ Future<String> run() async {
         case 'list':
           print('listing cables');
           req.response
-              .writeAll(fileCablesContent.map((e) => json.encode(e)), '\n');
+              .writeAll(fileCablesContent.values.map((e) => json.encode(e)), '\n');
           await req.response.close();
           break;
         case 'add':
           print('adding new cablle');
-          fileCablesContent.add(Cable.fromJson(
+          fileCablesContent[req.requestedUri.queryParameters['key']!] = (Cable.fromJson(
               json.decode(Utf8Decoder().convert(await req.last))));
           targetCablesFile.writeAsString(json.encode(fileCablesContent));
           req.response.close();
           break;
         case 'put':
-          String jsonString = '';
-          try {
-            print(
-                'putting fosc with key=${req.requestedUri.queryParameters['key']}');
-            jsonString = Utf8Decoder().convert(await req.last);
-            fileCablesContent[fileCablesContent.indexWhere((cable) =>
-                    cable.key == req.requestedUri.queryParameters['key'])] =
-                Cable.fromJson(json.decode(jsonString));
-            targetCablesFile.writeAsString(json.encode(fileCablesContent));
-          } catch (e) {
-            print('cable was not found with that key');
-            print('adding new cable');
-            fileCablesContent.add(Cable.fromJson(json.decode(jsonString)));
-            targetCablesFile.writeAsString(json.encode(fileCablesContent));
-          }
+          print(
+              'putting fosc with key=${req.requestedUri.queryParameters['key']}');
+          fileCablesContent[req.requestedUri.queryParameters['key']!] = (Cable.fromJson(
+              json.decode(Utf8Decoder().convert(await req.last))));
+          targetCablesFile.writeAsString(json.encode(fileCablesContent));
           break;
       }
     }
-    /*
-    try {
-      final offset =
-          int.parse(req.requestedUri.queryParameters['offset'] ?? '0');
-      final pageSize =
-          int.parse(req.requestedUri.queryParameters['pageSize'] ?? '10');
-      final sortIndex =
-          int.parse(req.requestedUri.queryParameters['sortIndex'] ?? '1');
-      final sortAsc =
-          int.parse(req.requestedUri.queryParameters['sortAsc'] ?? '1') == 1;
-
-      //fileFoscsContent.sort((a, b) => sortFoscs(a, b, sortIndex, sortAsc));
-      req.response.write(
-        jsonEncode(
-          ResponseModel(
-            fileFoscsContent.length,
-            fileFoscsContent.skip(offset).take(pageSize).toList(),
-          ),
-        ),
-      );
-    } catch (e) {
-      print('Something went wrong: $e');
-      req.response.statusCode = HttpStatus.internalServerError;
-    }
-    await req.response.close();*/
+    req.response.writeln('ok');
+    req.response.close();
   }
-  return '123';
+  return 'done.';
 }
